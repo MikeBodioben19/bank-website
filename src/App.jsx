aef7c10a14e9a5fc1000f100a9df6b58cd5c1d84
bodioben miguel <miguelbodio@gmail.com>
2022-09-01 02:07:39 +0200

    creation de la navbar et enregistrement des fichiers finaux

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
 import React from 'react'
-
-function App() {
+import styles from './style'
+import { Navbar,Hero,Billing,CardDeal,Business,Clients,CTA,Footer,Stats} from './Components'
+  const  App = () => {
 
   return (
-    <div className="App">
+    <div className="App bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+          <div className={`${styles.boxWidth}`}>
+            <Navbar/>
+          </div>
+      </div>
 
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          Hero
+        </div>
+      </div>
+
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+        
+        </div>
+      </div>
     </div>
   )
 }
